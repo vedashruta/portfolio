5d7d8dd52a8396c45e03e87a97ffbfdfa17a474b
vedashruta <vedashrutabhat@gmail.com>
2024-07-13 10:06:59 +0530

    [MNT] Updated scrool up functionality

--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -3,11 +3,11 @@ import "./scrollup.css";
 export const Scrollup = () => {
     window.addEventListener("scroll",function() {
         const scrollUp=document.querySelector(".scrollup");
-        if(this.scrollY >= 560)  scrollUp.classList.add("show-scroll");
+        if(this.scrollY >= 500)  scrollUp.classList.add("show-scroll");
         else scrollUp.classList.remove("show-scroll");
     });
   return (
-    <a href="#" className='scrollup'>
+    <a className='scrollup' onClick={Scrollup} href="#home">
         <i className='uil uil-arrow-up scrollup__icon'></i>
     </a>
   )
