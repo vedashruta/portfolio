c1599a741388276d57da4c04374bde0600893b75
vedashruta <vedashrutabhat@gmail.com>
2024-07-13 10:13:48 +0530

    Updated scroll to top functionality

--- src/components/scrollup/Scrollup.jsx
+++ src/components/scrollup/Scrollup.jsx
@@ -1,14 +1,24 @@
 import React from 'react'
 import "./scrollup.css";
+
+const scrollToTop = () => {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+}
+
 export const Scrollup = () => {
-    window.addEventListener("scroll",function() {
-        const scrollUp=document.querySelector(".scrollup");
-        if(this.scrollY >= 500)  scrollUp.classList.add("show-scroll");
-        else scrollUp.classList.remove("show-scroll");
-    });
+  window.addEventListener("scroll", function () {
+    const scrollUp = document.querySelector(".scrollup");
+    if (this.scrollY >= 500) 
+      scrollUp.classList.add("show-scroll");
+    else 
+      scrollUp.classList.remove("show-scroll");
+  });
   return (
-    <a className='scrollup' onClick={Scrollup} href="#home">
-        <i className='uil uil-arrow-up scrollup__icon'></i>
+    <a className='scrollup' onClick={scrollToTop} >
+      <i className='uil uil-arrow-up scrollup__icon'></i>
     </a>
   )
 }
